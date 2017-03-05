61a808f8e32e1025fc7d75075059cbb48cb00acb
vasilisa <demvv@mail.ru>
2017-03-06 02:26:44 +0300

    FFF

--- src/com/myorg/servlet/servlet/HelloServlet.java
+++ src/com/myorg/servlet/servlet/HelloServlet.java
@@ -53,6 +53,12 @@ public class HelloServlet extends HttpServlet {
 		out.println("<h4>Имя:</h4>");
 		out.println("<h4>Фамилия: </h4>");
 		out.println("<h4>Имя:</h4>");
+		
+		
+		
+		
+		
+		
 //////////////////////////////////////////////////
 		////////////////////
 
